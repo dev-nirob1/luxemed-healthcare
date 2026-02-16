1c56df23121be114427e42e7eb2225b8146d67d9
dev-nirob1 <dev.nirob1@gmail.com>
2026-02-16 20:21:28 +0600

    about section

--- src/zems/Front/Components/Section/AboutSection.vue
+++ src/zems/Front/Components/Section/AboutSection.vue
@@ -1,5 +1,44 @@
+<script setup lang="ts">
+import SectionTitle from "../Widget/SectionTitle.vue";
+</script>
+
 <template>
-  <section id="about" class="about-split">
+  <section class="about-section">
+    <div class="container">
+      <div class="medium-2 gap-2">
+        <div class="image">
+          <img
+            class="img1"
+            src="https://images.unsplash.com/photo-1631217868264-e5b90bb7e133?q=80&w=2091&auto=format&fit=crop"
+            alt=""
+          /><img
+            class="img2"
+            src="https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?q=80&w=2053&auto=format&fit=crop"
+            alt=""
+          />
+        </div>
+        <div class="content">
+          <SectionTitle title="Architecture of Healing" subTitle="Who We Are" />
+          <BaseParagraph>
+            LuxeMed isn't just a hospital; it's a testament to human ingenuity.
+            Designed by biophilic architects, our facility promotes healing
+            through connection with nature, light, and space.
+          </BaseParagraph>
+          <BaseParagraph>
+            Our team of global experts collaborates using a unified digital
+            cloud, ensuring that your health data travels with you, securely and
+            instantly.
+          </BaseParagraph>
+          <ul class="features">
+            <ListItem>100+ Sustainable Energy powered</ListItem>
+            <ListItem>Paperless Digital Workflows</ListItem>
+            <ListItem>Global Tele-Health Network</ListItem>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- <section id="about" class="about-split">
     <div class="about-image-stack">
       <img
         src="https://images.unsplash.com/photo-1631217868264-e5b90bb7e133?q=80&w=2091&auto=format&fit=crop"
@@ -11,7 +50,7 @@
       />
     </div>
     <div class="about-text">
-      <span class="section-label">Who We Are</span>
+      <span class="section-label">""</span>
       <h3>Architecture of Healing</h3>
       <p>
         LuxeMed isn't just a hospital; it's a testament to human ingenuity.
@@ -46,11 +85,40 @@
         </li>
       </ul>
     </div>
-  </section>
+  </section> -->
 </template>
 <style scoped>
+.about-section {
+  padding: 4.75rem 0;
+}
+.about-section .content p {
+  font-size: 1.15rem;
+}
+.about-section .image {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.image .img1,
+.image .img2 {
+  border-radius: 1rem;
+}
+.about-section .image .img1 {
+  width: 90%;
+  height: 100%;
+  object-fit: cover;
+}
+.about-section .image .img2 {
+  width: 260px;
+  height: 260px;
+  object-fit: cover;
+  position: absolute;
+  right: 0;
+  border: 5px solid var(--white-color);
+  top: 50%;
+}
 /* About (Asymetric) */
-.about-split {
+/* .about-split {
   display: grid;
   grid-template-columns: 1.2fr 0.8fr;
   gap: 80px;
@@ -116,7 +184,7 @@
   }
 
   .about-image-stack {
-    height: 450px; /* Reduced height for mobile */
+    height: 450px;
     margin-bottom: 20px;
   }
 
@@ -133,5 +201,5 @@
   .about-text h3 {
     font-size: 1.75rem;
   }
-}
+} */
 </style>
---
+++ src/zems/Front/Components/Widget/SectionTitle.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  subTitle: String,
+  title: String,
+});
+</script>
+<template>
+  <div class="section-title">
+    <span class="section-label">{{ subTitle }}</span>
+    <BaseTitle>{{ title }}</BaseTitle>
+  </div>
+</template>
+<style scoped>
+.section-title {
+  color: var(--secondary-color);
+}
+.section-title .section-label {
+  text-transform: uppercase;
+  font-size: 0.9rem;
+  letter-spacing: 0.1em;
+  font-weight: 600;
+  margin-bottom: 0.75rem;
+  display: block;
+}
+</style>
